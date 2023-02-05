26467663b358f49407c3097d4b475024fc13c577
ibhati0991 <53371983+ibhati0991@users.noreply.github.com>
2023-02-05 19:57:45 +0530

    2 files modified

--- index.ts
+++ index.ts
@@ -1,5 +1,23 @@
 // Import stylesheets
 import './style.css';
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCcmEk_LTg6LArbbMhk7Unpk8SyPszPfBo',
+  authDomain: 'esp32-car-aa3ea.firebaseapp.com',
+  databaseURL: 'https://esp32-car-aa3ea-default-rtdb.firebaseio.com',
+  projectId: 'esp32-car-aa3ea',
+  storageBucket: 'esp32-car-aa3ea.appspot.com',
+  messagingSenderId: '690319348381',
+  appId: '1:690319348381:web:f9c6e948a690e22e6b7a74',
+  measurementId: 'G-NDY321MGHQ',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Realtime Database and get a reference to the service
+const database = getDatabase(app);
 
 const joystickContainer: HTMLElement =
   document.getElementById('joystick-container');
@@ -16,10 +34,11 @@ joystickContainer.addEventListener('pointermove', (event) => {
     joystick.style.left = x + 'px';
     joystick.style.top = y + 'px';
     info.innerHTML = `<h3>X:${x} Y:${y}</h3>`;
+    writeUserData(x, y);
   }
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', `/?posX=${x}&posY=${y}`, true);
-  xhr.send();
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('GET', `/?posX=${x}&posY=${y}`, true);
+  // xhr.send();
 });
 
 joystickContainer.addEventListener('pointerout', () => {
@@ -28,9 +47,18 @@ joystickContainer.addEventListener('pointerout', () => {
   x = 0;
   y = 0;
   info.innerHTML = `<h3>X:${x} Y:${y}</h3>`;
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', '/?posX=${x}&posY=${y}', true);
-  xhr.send();
+  writeUserData(x, y);
+  // var xhr = new XMLHttpRequest();
+  // xhr.open('GET', '/?posX=${x}&posY=${y}', true);
+  // xhr.send();
 });
 
 info.innerHTML = `<h3>X:${x} Y:${y}</h3>`;
+
+function writeUserData(x, y) {
+  const db = getDatabase();
+  set(ref(db, 'positions/' + 'esp32Car'), {
+    x: x,
+    y: y,
+  });
+}
